746d6353b5cb9a7cb65495a39c1af9da3701889e
Vid Bukovec <vid.bukovec8361@gmail.com>
2019-08-07 13:45:07 +0200

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -6,5 +6,26 @@
    }
   $obj = new test();
 
+<<<<<<< Updated upstream
   $obj->xd();
+=======
+<<<<<<< Updated upstream
+echo "sample text";
+echo "can i eat ur ass?";
+=======
+<<<<<<< Updated upstream
+echo "sample text"
+echo "can i eat ur ass?"
+=======
+class test{
+    public function _consctructor(){
+         echo "Test";
+    }
+}
+
+$obj = new test();
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
  ?>
