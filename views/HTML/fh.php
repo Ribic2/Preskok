357c884c47c475852254f31a88997ab7ad55d1a7
David <david.seruga@gmail.com>
2019-08-08 15:21:23 +0200

    footer and header are now finished

--- views/HTML/fh.php
+++ views/HTML/fh.php
@@ -33,11 +33,43 @@
             </ul>
         </div>
     </nav>
-    
-    <!-- footer -->
-    
-
-    
+    <div style="
+        height:1500px;
+    "></div>
+    <!-- Footer -->
+    <footer class="bg-primary text-white">
+        <br>
+        <div class="clearfix">
+            <div class="float-left col-md-4">
+                <h1>Naše podjetje</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt illo reiciendis iste fuga atque molestiae. Sint blanditiis qui, doloribus nam. Aspernatur provident esse laborum minima sint quisquam dignissimos, nihil eius.</p>
+                <ul>
+                    <li><a href="" class="text-white">neka stran</a></li>
+                    <li><a href="" class="text-white">neka druga stran</a></li>
+                    <li><a href="" class="text-white">vec strani</a></li>
+                    <li><a href="" class="text-white">in še ena</a></li>
+                </ul>
+            </div>
+            <div class="float-left col-md-4">
+                <img src="https://www.stickpng.com/assets/images/59db69d33752880e93e16efc.png" alt="slika avta" style="width:100%">
+            </div>
+            <div class="float-left col-md-4">
+                <h1>Kontakt</h1>
+                <p>
+                    Tel.: 069 420 007 <br>
+                    Mail.: info@carmarket.com <br>
+                    Naslov.: Štukljeva ulica 40, 1000 Ljubljana, Slovenija <br>
+                    <br>
+                    Glavni šefe - Aleš Glogo (on že ve da ga iščeš :P)
+                </p>
+            </div>
+            
+        </div>
+        <div class="text-center">
+            To je moja spletna stran in ne pustim, da mi jo vzamete <br>
+            <a href="#" class="text-white">Copyright</a><br><br>
+        </div>
+    </footer>
     
 </body>
 </html>
