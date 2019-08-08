c559c3b625ed2199edcc96a32b7ac483c4c6bac6
Andraž Bajec <andraz.bajec27@gmail.com>
2019-08-08 15:07:58 +0200

    Added AURA routing

--- index.php
+++ index.php
@@ -1,15 +1,75 @@
-<?php
-require "vendor/autoload.php";
+<?php 
 
-include 'model/database.php';
+require __DIR__ . '/vendor/autoload.php';
 
-$pdo=\database\test::returndb();
+// create a server request object
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals
+    (
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
 
-//$pdo->query("insert into login values('cancer','pass');");
+// create the router container and get the routing map
+$routerContainer = new Aura\Router\RouterContainer();
+$map = $routerContainer->getMap();
 
-$result=$pdo->query("select * from login");
+// add a route to the map, and a handler for it
+$map->get('index', '/', function ($request) {
+    $response = new Zend\Diactoros\Response();
+    $response->getBody()->write("INDEX PAGE");
+    return $response;
+});
 
-$row=$result->fetchAll();
-echo $row[0][1];
 
-//echo(row['username']."<br>".row['password']);
+$map->get('home', '/home', function ($request) {
+    $response = new Zend\Diactoros\Response();
+    //$response->getBody()->write("HOME PAGE");
+    //return $response;
+    
+    exit;
+});
+
+/*
+$map->get('user.edit', '/user/edit', function ($request) {
+   $responseHandler("Text");
+});*/
+
+
+$responseHandler = function(string $html)
+{
+    $response = new Zend\Diactoros\Response();
+    $response->getBody()->write($html);
+    return $response;
+};
+// get the route matcher from the container ...
+$matcher = $routerContainer->getMatcher();
+
+// .. and try to match the request to a route.
+$route = $matcher->match($request);
+if (! $route) {
+    http_response_code(404);
+    exit;
+}
+
+// add route attributes to the request
+foreach ($route->attributes as $key => $val) {
+    $request = $request->withAttribute($key, $val);
+}
+
+// dispatch the request to the route handler.
+// (consider using https://github.com/auraphp/Aura.Dispatcher
+// in place of the one callable below.)
+$callable = $route->handler;
+$response = $callable($request);
+
+// emit the response
+foreach ($response->getHeaders() as $name => $values) {
+    foreach ($values as $value) {
+        header(sprintf('%s: %s', $name, $value), false);
+    }
+}
+http_response_code($response->getStatusCode());
+echo $response->getBody();
--- views/HTML/home.php
+++ views/HTML/home.php
@@ -16,6 +16,9 @@
         <form>
 
             <select name="brand">
+                <?php
+                include '../model/database.php';
+                ?>
                 <option value="volvo">Volvo</option>
                 <option value="saab">Saab</option>
                 <option value="mercedes">Mercedes</option>
