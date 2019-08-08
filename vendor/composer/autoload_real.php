59f0b4c09d11a01da9fbf0d868008ead75131f18
Andraž Bajec <andraz.bajec27@gmail.com>
2019-08-08 14:16:03 +0200

    Added database connection namespace

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit1c656804d6b474184946d5303aeb1592
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit1c656804d6b474184946d5303aeb1592::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire1c656804d6b474184946d5303aeb1592($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire1c656804d6b474184946d5303aeb1592($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
