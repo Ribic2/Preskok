38b7dc8707dfa4e7b1883a4763847b98c6c8e62c
Aleks <glogovsek.aleks@gmail.com>
2019-08-08 07:05:01 -0700

    Izbera Registracije koncana

---
+++ views/HTML/IzberaRegistracije.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Prijava</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="../css/izberaregistracije.css">
+    </head>
+    <body>
+        <div class="d-md-flex h-md-100 align-items-center">
+            <div class="col-md-6 p-0 bg-indigo h-md-100">
+    <div class="float-right text-white d-md-flex align-items-center h-100 text-center justify-content-center">
+        <div class="logoarea pt-5 pb-5">
+        <div class="container">
+        <div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Ali želite objaviti oglas kot POSAMEZNIK?</h3>
+			</div>
+			 <div class="card-body h-50">
+                    <div style="text-align: left">
+                    <p>Prednosti registracije za fizične osebe / posameznike:</p>
+                        <li>Objava BREZPLAČNEGA oglasa (*)</li>
+                        <li>Lahko si shranite PRILJUBLJENE kriterije iskanja</li>
+                        <li>Možnost prednastavitve željenega načina SORTIRANJA</li>
+                        <li>Možnost PARKIRANJA priljubljenih oglasov</li>
+                        <li>Možnost PARKIRANJA priljubljenih kriterijev iskanja</li>
+
+                    <p>(*) Število objav je v izogib dela na črno omejeno na največ 2 brezplačna oglasa v posamezni rubriki istočasno.</p>
+
+                        <p>Primerno za fizične osebe / posameznike, kakor tudi za podjetja, ki želijo prodati lastno osnovno sredstvo</p></div>
+			</div>
+            <div class="card-footer">
+                    <div class="form-group">
+						<input type="submit" value="Želim se registrerati kot POSAMEZNIK" class="btn float-center login_btn">
+					</div>
+                </div>
+		</div>
+	</div>
+</div>
+        </div>
+                </div>
+            </div>
+                     <div class="col-md-6 p-0 bg-indigo h-md-100">
+    <div class="text-white float-left d-md-flex align-items-center h-100 text-center justify-content-center">
+        <div class="logoarea pt-5 pb-5">
+        <div class="container">
+        <div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Ali želite oglaševati kot TRGOVEC?</h3>
+			</div>
+			<div class="card-body h-50">
+                    <div style="text-align: left">
+                    <p>Zakaj se registrirati kot trgovec?</p>
+                        <li>Kadar opravljate registrirano dejavnost, in želite objaviti oglase za prodajo vozil, opreme ali mehanizacije (*)</li>
+                        <li>Lahko si shranite PRILJUBLJENE kriterije iskanja</li>
+                        <li>Brezplačno 30 dnevno testno obdobje</li>
+                        <li>Naprednejše možnosti objave oglasa (* opcija)</li>
+                        <li>Statistični pregled objave (*opcija)</li><br>
+                    <p>(*) Sem sodijo vse pravne osebe, tudi samostojni podjetniki, društva, zavodi, združenja, agencije ..., ki želji oglaševati prodajo ali najem v okviru registrirane dejavnosti.</p></div>
+			</div>
+            <div class="card-footer">
+                    <div class="form-group">
+						<input type="submit" value="Želim se registrerati kot POSAMEZNIK" class="btn float-center login_btn">
+					</div>
+                </div>
+		</div>
+	</div>
+</div>
+        </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
