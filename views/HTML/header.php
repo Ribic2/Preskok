8b1582c32055f1535a764c058c9c5f90e2ecacf2
David <david.seruga@gmail.com>
2019-08-08 15:36:30 +0200

    locil footer in header na 2 datoteki

---
+++ views/HTML/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>fh</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <!-- header -->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <!-- Brand/logo -->
+        <a class="navbar-brand" href="#">
+            <img src="https://brandmark.io/logo-rank/random/apple.png" alt="CarMarket" style="width:40px;">
+        </a>
+
+        <!-- Links -->
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#"><span class="glyphicon">&#xe081;</span> Objavi oglas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Prijava</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
