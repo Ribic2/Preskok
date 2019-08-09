e818c9057c573e9a799e218dfc5bc0f055c34efc
matej <matejmarolt27@gmail.com>
2019-08-09 10:09:59 +0200

    list ad

--- views/HTML/list.php
+++ views/HTML/list.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Home</title>
+    <title>CAR LIST</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -10,12 +10,21 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="../css/list.css">
+
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-xl-3">
-                <div class="">
+                <div class="card"> <!--  mal spremenit za filtre, da nebo enaku ku adi za avte  -->
+
+                    <div class="card-body">
+
+                        <h4 class="card-title"><a>Filtri</a></h4>
+
+
+
+                    </div>
 
                 </div>
 
@@ -23,6 +32,45 @@
             </div>
 
             <div class="col-xl-9">
+                <div class="card">
+
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-xl-3">
+                                tuki bo slika
+                                <!-- img -->
+                            </div>
+                            <div class="col-xl-9">
+                                <!-- info -->
+                                <div class="row">
+                                    <div class="col-xl-9">
+
+                                        <h5 class="card-title"><a>Ime avta</a></h5>
+
+                                        <div class="info_car">
+                                            -osnovni podatki o avtu<br>
+                                            -osnovni podatki o avtu<br>
+                                            -osnovni podatki o avtu<br>
+                                            -osnovni podatki o avtu
+                                        </div>
+                                        <br>
+                                        <a class="btn login_btn"><i class="fas fa-clone left"></i> View AD</a>
+                                    </div>
+                                    <div class="col-xl-3">
+                                        <h5 class="card-title"><a>cena:PHPshit</a></h5>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <h6 class="card-title"><a>Tuki je David hotu KRAJ</a></h6>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
 
             </div>
         </div>
