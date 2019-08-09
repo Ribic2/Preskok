079eca6dfa8f23bcd1ff5235de6711af05b8f7f7
Lovro <lovro.krizan41@gmail.com>
2019-08-09 15:37:23 +0200

    ardhd

--- views/HTML/register.php
+++ views/HTML/register.php
@@ -37,7 +37,9 @@
 			</div>
             <div class="card-footer">
                     <div class="form-group">
+                        <a href="register.php">
 						<input type="submit" onclick="personal.php" value="Želim se registrirati kot POSAMEZNIK" class="btn float-center login_btn">
+                        </a>
 					</div>
                 </div>
 		</div>
@@ -67,8 +69,10 @@
 			</div>
             <div class="card-footer">
                     <div class="form-group">
+                        <a href="personal.php">
 						<input type="submit" value="Želim se registrirati kot TRGOVEC" class="btn float-center login_btn">
-					</div>
+                        </a>
+                    </div>
                 </div>
 		</div>
 	</div>
