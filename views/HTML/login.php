bc62124bd254a4f0d3ce01cb7bf4bd2f864bb809
Andraž Bajec <andraz.bajec27@gmail.com>
2019-08-09 09:23:34 +0200

    Removed gitkraken error

--- views/HTML/login.php
+++ views/HTML/login.php
@@ -33,33 +33,26 @@ if(isset($_POST['username'])&&isset($_POST['password']))
     </head>
     <body id = "body">
         <div class="container">
-        <div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
-			</div>
-			<div class="card-body">
-				<form method="post" action="prijava.php">
+            <div class="d-flex justify-content-center h-100">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Sign In</h3>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="prijava.php">
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-<<<<<<< HEAD:views/HTML/login.php
-						<input type="text" class="form-control" placeholder="username" name="username">
-=======
-						<input type="text" class="form-control" placeholder="username" id = "username">
->>>>>>> dbea11bad5044c72fd6d8c2761bd1addb661bf45:views/HTML/Prijava.php
-						
-					</div>
+                        <input type="text" class="form-control" placeholder="username" name="username">
+                        <input type="text" class="form-control" placeholder="username" id = "username">
+                    </div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-<<<<<<< HEAD:views/HTML/login.php
 						<input type="password" class="form-control" placeholder="password" name="password">
-=======
 						<input type="password" class="form-control" placeholder="password" id = "password">
->>>>>>> dbea11bad5044c72fd6d8c2761bd1addb661bf45:views/HTML/Prijava.php
 					</div>
 					<div class="row align-items-center remember">
 						<input type="checkbox">Remember Me
